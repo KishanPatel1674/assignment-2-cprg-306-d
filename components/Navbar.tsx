0ae6c9a83aa18e265973416721977babfa905b81
KishanPatel1674 <kishanpatel1935@gmail.com>
2025-06-17 22:26:20 -0600

    Update Navbar.tsx
    we add the postion for  stick on top

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,16 @@
 export default function Navbar() {
   return (
-    <nav>
+  <nav
+      style={{
+        position: 'fixed',
+        top: 0,
+        width: '100%',
+        zIndex: 1000,
+        backgroundColor: '#007BFF',
+        padding: '10px 20px',
+        color: 'white',
+      }}
+    >
       <h1>New Generation High School</h1>
     </nav>
   );
