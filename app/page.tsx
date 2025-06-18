a928424aeb9aa326dd70dc8fd575a326fcf5da73
KishanPatel1674 <kishanpatel1935@gmail.com>
2025-06-17 21:27:06 -0600

    Update and add detail ways of student information and function  page.tsx
    we used  client server  in page.tx

---
+++ app/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import React, { useState } from "react";
+import StudentList from "../components/StudentList";
+import AddStudentForm from "../components/AddStudentForm";
+import { Student } from "../types";
+
+export default function HomePage() {
+  const [students, setStudents] = useState<Student[]>([
+    { firstName: "Anandjit", lastName: "Kaur", dob: "2007-05-12", grade: "10" },
+    { firstName: "Mayur", lastName: "Tirkar", dob: "2006-11-23", grade: "11" },
+    { firstName: "Kishan", lastName: "Patel", dob: "2008-01-03", grade: "9" },
+    { firstName: "Emily", lastName: "Davis", dob: "2007-01-16", grade: "12" },
+    { firstName: "Michael", lastName: "Brown", dob: "2006-09-30", grade: "10" },
+  ]);
+
+  function addStudent(newStudent: Student) {
+    setStudents([...students, newStudent]);
+  }
+
+  return (
+    <main>
+      <h1 style={{ textAlign: "center", color: "#333" }}>Student List</h1>
+      <AddStudentForm onAddStudent={addStudent} />
+      <StudentList students={students} />
+    </main>
+  );
+}
