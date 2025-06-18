11e31715fde7c112c1ecce38a0b4ef381e209f8a
KishanPatel1674 <kishanpatel1935@gmail.com>
2025-06-17 22:04:48 -0600

    Update page.tsx
    we just remove double inverted comma (")  because that was string we added on types.js now its number.

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,11 @@ import { Student } from "../types";
 
 export default function HomePage() {
   const [students, setStudents] = useState<Student[]>([
-    { firstName: "Anandjit", lastName: "Kaur", dob: "2007-05-12", grade: "10" },
-    { firstName: "Mayur", lastName: "Tirkar", dob: "2006-11-23", grade: "11" },
-    { firstName: "Kishan", lastName: "Patel", dob: "2008-01-03", grade: "9" },
-    { firstName: "Emily", lastName: "Davis", dob: "2007-01-16", grade: "12" },
-    { firstName: "Michael", lastName: "Brown", dob: "2006-09-30", grade: "10" },
+    { firstName: "Anandjit", lastName: "Kaur", dob: "2007-05-12", grade: 10 },
+    { firstName: "Mayur", lastName: "Tirkar", dob: "2006-11-23", grade: 11 },
+    { firstName: "Kishan", lastName: "Patel", dob: "2008-01-03", grade: 9 },
+    { firstName: "Emily", lastName: "Davis", dob: "2007-01-16", grade: 12 },
+    { firstName: "Michael", lastName: "Brown", dob: "2006-09-30", grade: 10 },
   ]);
 
   function addStudent(newStudent: Student) {
