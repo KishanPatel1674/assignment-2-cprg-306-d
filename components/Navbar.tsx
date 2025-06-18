1e966292ee163fe606d68dbf8edb0edf5b8ba6d0
KishanPatel1674 <kishanpatel1935@gmail.com>
2025-06-17 22:38:13 -0600

    Update Navbar.tsx
    fixing the padding and added left for look border to border complete

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,6 +4,7 @@ export default function Navbar() {
       style={{
         position: 'fixed',
         top: 0,
+        left: 0,
         width: '100%',
         zIndex: 1000,
         backgroundColor: '#007BFF',
